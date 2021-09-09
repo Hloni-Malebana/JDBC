bff4f665511e6fce4aa891e1dc731fb680600c74
lehlohonolo <lehlohonolo@lehlohonolo-HP-EliteBook-2540p>
2021-09-09 14:32:06 +0200

    There is a problem authenticating/authorizing access to the sql server.
    I tried changing the password for the sql server and writing it down on
    a piece of paper but it still does not produce a different outcome.Even
    if I leave the password field empty.

--- JDBC/src/JDBCPractice/jdbcPractice.java
+++ JDBC/src/JDBCPractice/jdbcPractice.java
@@ -14,13 +14,10 @@ import java.sql.*;
 public class jdbcPractice {
 
 	public static void main(String[] args) throws Exception {
-		final String url = "jdbc:mysql://local:3306/Student";
-		final String userName = "root";
-		final String passWord = "root";
-		
+
 		// TODO Auto-generated method stub
 		try {
-		System.out.println("Connecting...");
+		System.out.println("Loading to the com.mysql.cj.jdbc.Driver driver.");
 		
 		/*
 		 * Load class called com.mysql.jdbc.Driver
@@ -44,16 +41,15 @@ public class jdbcPractice {
 		 * 
 		 */
 		
-		Connection con = DriverManager.getConnection(url,userName,passWord);
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Student","root","nopassword");
+		
 		System.out.println("Step 2 : Now that the jvm has connected to the sql server \n"
 				+ "using port 3306 and the password and username root root,\n"
 				+ "the jvm will read information from the Student database.");
-		
 
-		/* Instantiate a con object,that is a child of the Statement
+		/* Instantiate a con object,thatz is a child of the Statement
 		 * class,then call the createStatement method.
 		 */
-
 		
 		Statement st = con.createStatement();
 		ResultSet rs = st.executeQuery("select * from Student where Roll_no=1");
@@ -70,12 +66,9 @@ public class jdbcPractice {
 		System.out.println(roll +" "+ name +" "+ city +" "+ surname);
 		}
 		st.close();
-		con.close();
-		
+		con.close();	
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
-
 }
-
