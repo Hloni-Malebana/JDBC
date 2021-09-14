1321611cd9b90d770b8cec5b0fb52f2fde1917ed
lehlohonolo <lehlohonolo@lehlohonolo-HP-EliteBook-2540p>
2021-09-14 12:46:46 +0200

    Unable to connect to the Student database
    I think the reason I am not able to connect to the Student database is
    because the IDE is not evaluating the url of the sql server and the
    Student database.

--- JDBC/src/JDBCPractice/jdbcPractice.java
+++ JDBC/src/JDBCPractice/jdbcPractice.java
@@ -14,61 +14,67 @@ import java.sql.*;
 public class jdbcPractice {
 
 	public static void main(String[] args) throws Exception {
+		
+		String username = "root";
+		String password = "root";
+		String url = "jdbc:mysql://localhost";
 
 		// TODO Auto-generated method stub
 		try {
-		System.out.println("Loading to the com.mysql.cj.jdbc.Driver driver.");
+			System.out.println("Step 1 : Loading to the com.mysql.cj.jdbc.Driver driver.");
 		
-		/*
-		 * Load class called com.mysql.jdbc.Driver
-		 * this driver is sitting in the 
-		 * mysql-connector-java-8.0.26.jar file.
-		 */
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		System.out.println("Step 1 :Now that the driver has loaded,the jvm will attempt to connect to the sql database.");
-		/* Instantiate a Driver Manager object, a child of the 
-		 * Connection class,then call the getConnection method
-		 * which will establish the connection between this program
-		 * and the sql database
-		 */
-		/*
-		 * The url parameter of the getConnection() method is a
-		 * database URL that specifies the subprotocol 
-		 * (the database connectivity mechanism), the database
-		 * or database server identifier, and a list of 
-		 * properties. A second parameter to the getConnection() method
-		 * , property,is the property list.
-		 * 
-		 */
+			/*
+			 * Load class called com.mysql.jdbc.Driver
+			 * this driver is sitting in the 
+			 * mysql-connector-java-8.0.26.jar file.
+			 */
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("\nStep 2 : Now that the driver has loaded,the jvm will attempt to connect to the Student sql database.");
+			/* Instantiate a Driver Manager object, a child of the 
+			 * Connection class,then call the getConnection method
+			 * which will establish the connection between this program
+			 * and the sql database
+			 */
+			/*
+			 * The url parameter of the getConnection() method is a
+			 * database URL that specifies the subprotocol 
+			 * (the database connectivity mechanism), the database
+			 * or database server identifier, and a list of 
+			 * properties. A second parameter to the getConnection() method
+			 * , property,is the property list.
+			 * 
+			 */
 		
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Student","hloni","Hl0ni@1447");
 		
-		System.out.println("Step 2 : Now that the jvm has connected to the sql server \n"
+			Connection con = DriverManager.getConnection(url,username,password);
+		
+			System.out.println("Step 2 : Now that the jvm has connected to the sql server \n"
 				+ "using port 3306 and the password and username root root,\n"
 				+ "the jvm will read information from the Student database.");
 
-		/* Instantiate a con object,thatz is a child of the Statement
-		 * class,then call the createStatement method.
-		 */
+			/* Instantiate a con object,thatz is a child of the Statement
+			 * class,then call the createStatement method.
+			 */
 		
-		Statement st = con.createStatement();
-		ResultSet rs = st.executeQuery("select * from Student where Roll_no=1");
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery("select * from Student where Roll_no=1");
 		
-		System.out.println("Step 3...");
+			System.out.println("Step 3...");
 		
-		while(rs.next()) {
+			while(rs.next()) {
 		
-		int roll = rs.getInt("Roll no");
-		String name = rs.getString("Name");
-		String city = rs.getString("City");
-		String surname = rs.getString("Surname");
+				int roll = rs.getInt("Roll no");
+				String name = rs.getString("Name");
+				String city = rs.getString("City");
+				String surname = rs.getString("Surname");
 		
-		System.out.println(roll +" "+ name +" "+ city +" "+ surname);
-		}
-		st.close();
-		con.close();	
-		}catch(Exception e){
-			e.printStackTrace();
+				System.out.println(roll +" "+ name +" "+ city +" "+ surname);
+			}
+			st.close();
+			con.close();	
+			}catch(Exception e){
+				System.out.println("\nSomething went wrong.\n \nError : "+e.getMessage());
+				
+			}
 		}
 	}
-}
